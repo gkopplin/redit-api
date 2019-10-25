c7aff982807a5618013fcb669cd3b50b27ee86bc
Mingjing Tang <tmjcsh@gmail.com>
2019-10-25 11:11:07 -0400

    user service test signup is working

---
+++ redit-api/src/test/java/com/ga/service/UserServiceTest.java
@@ -0,0 +1,82 @@
+package com.ga.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.ga.config.JwtUtil;
+import com.ga.dao.UserDao;
+import com.ga.entity.User;
+
+public class UserServiceTest {
+	@Mock
+	UserDao userDao;
+	
+	@Mock
+	private JwtUtil jwtUtil;
+	
+	@Mock
+    private PasswordEncoder bCryptPasswordEncoder;
+	
+	@InjectMocks
+	private User user;
+	
+    @InjectMocks
+    private UserServiceImpl userService;
+	
+    @Before
+    public void initMocks() {
+        MockitoAnnotations.initMocks(this);
+    }
+	
+	@Before 
+	public void initializeDummyUser() {
+		user.setUserId(1L);
+		user.setUsername("batman");
+		user.setEmail("batman@ga.com");
+		user.setPassword("bat");
+	}
+	
+	@Test
+	public void signup_ReturnsJwt_Success() {
+		String expectedToken = "12345";
+		String expectedUsername = "batman";
+		
+		when(userDao.signup(any())).thenReturn(user);
+        when(userDao.getUserByUsername(anyString())).thenReturn(user);
+        when(jwtUtil.generateToken(any())).thenReturn(expectedToken);
+        when(bCryptPasswordEncoder.encode(user.getPassword())).thenReturn("bat");
+        
+        String actualToken = userService.signup(user);
+        
+        assertEquals(actualToken, expectedToken);
+	}
+	
+	 @Test
+	    public void signup_UserNotFound_Error() {
+	    	
+	    	User tempUser = user;
+	    	tempUser.setUserId(null);
+
+	        when(userDao.signup(any())).thenReturn(tempUser);
+
+	        String token = userService.signup(user);
+
+	        assertEquals(token, null);
+	    }
+	 
+	 
+	 
+	 
+	
+	
+
+}
