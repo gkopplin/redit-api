203d394a4e7e3b321867f7e83d9a3c8d1843cf9d
Mingjing Tang <tmjcsh@gmail.com>
2019-10-27 13:57:07 -0400

    add user entit test

---
+++ redit-api/src/test/java/com/ga/entity/userEntityTest.java
@@ -0,0 +1,54 @@
+package com.ga.entity;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+
+
+public class userEntityTest {
+	@Rule
+	public MockitoRule rule = MockitoJUnit.rule();
+	
+	@InjectMocks
+	private User user;
+	
+	
+	@Before
+	public void init() {
+		List<Comment> comments = new ArrayList<Comment>();
+		List<Post> posts = new ArrayList<Post>();
+		
+		user.setUserId(1L);
+		user.setUsername("batman");
+		user.setEmail("batman@ga.com");
+		user.setAddress("batman st");
+		user.setMobile("000-000-0000");
+		user.setAddlEmail("batman2@ga.com");
+		user.setPassword("bat");
+		user.setComments(comments);
+		user.setPosts(posts);
+	}
+	
+	@Test
+	public void getUserId_Success() {
+		assertNotNull("Test returned null object, expected non-null", user.getUserId());
+	}
+	
+	public void getUsername_Success() {
+		assertNotNull("Test returned null object, expected non-null", user.getUsername());
+	}
+	
+	public void getUserEmail_Success() {
+		assertNotNull("Test returned null object, expected non-null", user.getEmail());
+	}
+
+}
